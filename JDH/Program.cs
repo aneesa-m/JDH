7efaed9026df612221ae3ed6f6e79298d1e69db4
Aneesa Mukhtar <Aneesa.Mukhtar@sqs.com>
2019-07-18 23:06:21 +0100

    Add project files.

---
+++ JDH/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JDH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hey Josh! Click Enter to see my secret message to you");
+            Console.ReadLine();
+            Console.WriteLine("I miss you! <3");
+            Console.ReadLine();
+            Console.WriteLine("The End");
+            Console.ReadLine();
+        }
+    }
+}
